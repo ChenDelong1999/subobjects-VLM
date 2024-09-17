d64272071e6c7b5426d2ceebe0af0be79f438781
ChenDelong1999 <673939938@qq.com>
2024-09-17 23:00:04 +0800

    automatically get feature_channels according to specified stage in HFAutoBackbone initialization

--- model/vision_encoders/hf_autobackbone.py
+++ model/vision_encoders/hf_autobackbone.py
@@ -9,8 +9,9 @@ class HFAutoBackbone(nn.Module):
 
         # check if the model_name contains two '/'
         if model_name.count('/') > 1:
-            out_features = [model_name.split('/')[-1]]
+            out_features = model_name.split('/')[-1]
             model_name = model_name.replace(f'/{out_features}', '')
+            out_features = [out_features]
         else:
             out_features = None
 
@@ -28,11 +29,15 @@ class HFAutoBackbone(nn.Module):
 
         self.model = transformers.AutoBackbone.from_pretrained(
             model_name, out_features=out_features).eval()
-
+        
         if hasattr(self.model.config, 'hidden_size'):
             self.feature_channels = self.model.config.hidden_size
         elif hasattr(self.model.config, 'hidden_sizes'):
-            self.feature_channels = self.model.config.hidden_sizes[-1]
+            if out_features is None:
+                stage_idx = -1
+            else:
+                stage_idx = self.model.config.stage_names.index(out_features[0]) - len(self.model.config.stage_names)
+            self.feature_channels = self.model.config.hidden_sizes[stage_idx]
     
     @torch.no_grad()
     def forward(self, images):
