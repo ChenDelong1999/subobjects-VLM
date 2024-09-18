905e66720cb9e4d5e52f59e2c94b101a11a63d2f
ChenDelong1999 <673939938@qq.com>
2024-09-18 19:28:06 +0800

    Update VisualTokenEmbedding model with improved ROI cropping-flattening pooling

--- model/visual_token_embedding.py
+++ model/visual_token_embedding.py
@@ -1,16 +1,21 @@
 import numpy as np
 import cv2
 import torch
+import torch.nn.functional as F
 from torch import nn
+import torchvision.ops as ops
 
 from .vision_encoders.rgb_pixel import RGBPixel
 from .vision_encoders.diffusers_vae import DiffusersVAE
 from .vision_encoders.hf_autobackbone import HFAutoBackbone
 from .vision_encoders.timm_backbone import TimmBackbone
 
-from timm.layers.attention_pool2d import RotAttentionPool2d, apply_rot_embed
-import torch.nn as nn
-
+vision_encoder_registry = {
+    'hf_autobacbone': HFAutoBackbone,
+    'diffusers_vae': DiffusersVAE,
+    'rgb_pixel': RGBPixel,
+    'timm_backbone': TimmBackbone
+}
 
 class VisualTokenEmbedding(torch.nn.Module):
 
@@ -18,112 +23,133 @@ class VisualTokenEmbedding(torch.nn.Module):
         super(VisualTokenEmbedding, self).__init__()
 
         self.config = config
-
-        type_to_class = {
-            'hf_autobacbone': HFAutoBackbone,
-            'diffusers_vae': DiffusersVAE,
-            'rgb_pixel': RGBPixel,
-            'timm_backbone': TimmBackbone
-        }
-
-        if config.vision_encoder_type in type_to_class:
-            self.vision_encoder = type_to_class[config.vision_encoder_type](
+        if config.vision_encoder_type in vision_encoder_registry:
+            self.vision_encoder = vision_encoder_registry[config.vision_encoder_type](
                 model_name=config.vision_encoder_name, 
                 image_resolution=config.image_resolution
                 ).eval()
         else:
             raise NotImplementedError
-        
-        self.attn_pool = RotAttentionPool2dWithNorm(
-            in_features=self.vision_encoder.feature_channels,
-            out_features=config.embedding_dim,
-            num_heads=config.num_heads
-        )
 
 
-    def forward(self, batch_images, batch_masks):
-
-        with torch.no_grad():
-            batch_features = self.vision_encoder(batch_images)
-
-        feat_res = batch_features.shape[-1]
-        num_masks = batch_masks.shape[1]
-
-        # batch_masks: (N, num_masks, image_resolution, image_resolution)
-        # downsample masks to feature resolution
-        batch_masks = nn.functional.interpolate(
-            torch.tensor(batch_masks).float(),  
-            size=(feat_res, feat_res),
-            mode='bilinear',
-            align_corners=False
-        ).to(batch_features.device).to(batch_features.dtype)
-        
-        # batch_features:   (N, C,              feat_res, feat_res)
-        # batch_masks:      (N, num_masks,      feat_res, feat_res)
-        # Apply mask for each feature map
-        # masked_features:  (N, num_masks, C,   feat_res, feat_res)
-        masked_features = batch_features.unsqueeze(1) * batch_masks.unsqueeze(2)
-
-        # (N, num_masks, C, feat_res, feat_res) -> (N * num_masks, C, feat_res, feat_res)
-        masked_features = masked_features.view(-1, *masked_features.shape[2:])
-        embeddings = self.attn_pool(masked_features) 
-        
-        # (N * num_masks, embedding_dim) -> (N, num_masks, embedding_dim)
-        embeddings = embeddings.view(-1, num_masks, self.config.embedding_dim)
-        return embeddings
-
-    
     @property
     def dtype(self):
         return self.vision_encoder.dtype
     
+
     @property
     def device(self):
         return self.vision_encoder.device
     
 
-class RotAttentionPool2dWithNorm(RotAttentionPool2d):
+    def forward(self, batch_images, batch_masks):
+        """
+        Forward pass of the visual token embedding model.
+        Args:
+            batch_images (list): A list of PIL images.
+            batch_masks (np.ndarray): A numpy array of shape (N, M, H, W) containing binary masks.
 
-    # https://github.com/huggingface/pytorch-image-models/blob/ee5b1e8217134e9f016a0086b793c34abb721216/timm/layers/attention_pool2d.py#L22
+        Returns:
+            roi_boxes  (torch.Tensor): A tensor of shape (N, M, 4) containing the bounding boxes of each mask.
+            roi_masks  (torch.Tensor): A tensor of shape (N, M, token_resolution, token_resolution) containing the cropped masks.
+            embeddings (torch.Tensor): A tensor of shape (N, M, channels * token_resolution * token_resolution) containing the visual token embeddings.
+        """
 
-    def forward(self, x, pre_logits: bool = False):
+        with torch.no_grad():
+            batch_features = self.vision_encoder(batch_images)
 
-        # x: (bs, feature_channels, feat_res, feat_res)
-        B, _, H, W = x.shape
-        N = H * W
-        x = x.flatten(2).transpose(1, 2)
-        if self.cls_token is None:
-            avg_pooled = x.mean(1, keepdim=True)
-            # Need to normalize the avg_pooled query since mask size varies
-            avg_pooled = nn.functional.normalize(avg_pooled, p=2, dim=1)
-            x = torch.cat([avg_pooled, x], dim=1)
-        else:
-            x = torch.cat([self.cls_token.expand(x.shape[0], -1, -1), x], dim=1)
-        if self.qkv is None:
-            q = self.q(x).reshape(B, N + 1, self.num_heads, self.head_dim).transpose(1, 2)
-            k = self.k(x).reshape(B, N + 1, self.num_heads, self.head_dim).transpose(1, 2)
-            v = self.v(x).reshape(B, N + 1, self.num_heads, self.head_dim).transpose(1, 2)
-        else:
-            x = self.qkv(x).reshape(B, N + 1, 3, self.num_heads, self.head_dim).permute(2, 0, 3, 1, 4)
-            q, k, v = x.unbind(0)
+        batch_masks = torch.tensor(batch_masks).float().to(batch_features.device).to(batch_features.dtype)
 
-        rse, rce = self.pos_embed.get_embed((H, W))
-        q = torch.cat([q[:, :, :1, :], apply_rot_embed(q[:, :, 1:, :], rse, rce)], dim=2).type_as(v)
-        k = torch.cat([k[:, :, :1, :], apply_rot_embed(k[:, :, 1:, :], rse, rce)], dim=2).type_as(v)
+        roi_boxes, roi_masks, embeddings = self.mask_roi_pooling(batch_features, batch_masks)
+        return roi_boxes, roi_masks, embeddings
+    
 
-        if self.fused_attn:
-            x = nn.functional.scaled_dot_product_attention(q, k, v)
-        else:
-            q = q * self.scale
-            attn = q @ k.transpose(-2, -1)
-            attn = attn.softmax(dim=-1)
-            x = attn @ v
-        x = x.transpose(1, 2).reshape(B, N + 1, -1)
-        x = self.drop(x)
-        if pre_logits:
-            x = self._pool(x, H, W)
-            return x
-        x = self.proj(x)
-        x = self._pool(x, H, W)
-        return x
+    def mask_roi_pooling(self, batch_features, batch_masks):
+        N, C, H, W = batch_features.shape
+        M = batch_masks.shape[1]
 
+        # Downsample masks to feature map resolution -> (N, M, H, W)
+        batch_masks = F.interpolate(
+            batch_masks, size=(H, W),
+            mode='bilinear', align_corners=False
+        )
+
+        # Get ROI boxes for each mask
+        roi_boxes = self.get_roi_boxes_from_masks(batch_masks)
+
+        # Perform ROIAlign for features
+        roi_features = ops.roi_align(
+            batch_features, 
+            roi_boxes, 
+            output_size=(self.config.token_resolution, self.config.token_resolution),
+            sampling_ratio=1
+            ).view(N, M, C, self.config.token_resolution, self.config.token_resolution)
+
+        
+        # Perform ROIAlign for masks
+        roi_masks = self.crop_roi_masks(
+            batch_masks.unsqueeze(2).repeat(1, 1, C, 1, 1), 
+            roi_boxes, self.config.token_resolution
+        )
+
+        # Apply mask to the features and flatten to embeddings
+        roi_features = roi_features * roi_masks
+        embeddings = roi_features.view(N, M, -1)
+
+        return torch.stack(roi_boxes), roi_masks[:, :, 0], embeddings
+    
+
+    def get_roi_boxes_from_masks(self, batch_masks):
+        binary_masks = (batch_masks > 0).float()
+        
+        N, M, H, W = batch_masks.shape
+        roi_boxes = []
+        
+        # Compute bounding boxes for each mask in the batch
+        for n in range(N):
+            sample_roi_boxes = []
+            for m in range(M):
+                mask = binary_masks[n, m]
+                if mask.sum() > 0:
+                    y_indices, x_indices = torch.where(mask)
+                    box = torch.tensor([
+                        x_indices.min(), y_indices.min(),
+                        x_indices.max(), y_indices.max()
+                    ], dtype=torch.int32, device=batch_masks.device)
+                    
+                    sample_roi_boxes.append([box[0], box[1], box[2], box[3]])
+                else:
+                    sample_roi_boxes.append([0, 0, W-1, H-1])
+
+            sample_roi_boxes= torch.tensor(sample_roi_boxes, dtype=torch.float32, device=batch_masks.device)
+            roi_boxes.append(sample_roi_boxes)
+        
+        return roi_boxes
+        
+
+    def crop_roi_masks(self, batch_masks, roi_boxes, token_resolution):
+        N, M, C, H, W = batch_masks.shape
+
+        # Prepare the output tensor
+        cropped_masks = torch.zeros(
+            N, M, C, token_resolution, token_resolution, 
+            device=batch_masks.device, dtype=batch_masks.dtype
+            )
+        
+        for n in range(N):
+            for m in range(M):
+                x1, y1, x2, y2 = roi_boxes[n][m].long()
+                x1, y1 = max(0, x1), max(0, y1)
+                x2, y2 = min(W - 1, x2), min(H - 1, y2)
+
+                cropped = batch_masks[n, m, :, y1:y2+1, x1:x2+1]
+                resized = F.interpolate(
+                    cropped.unsqueeze(0),  # Add batch dimension for interpolate
+                    size=(token_resolution, token_resolution),
+                    mode='bilinear',
+                    align_corners=False
+                ).squeeze(0)
+
+                cropped_masks[n, m] = resized
+        return cropped_masks
+    
