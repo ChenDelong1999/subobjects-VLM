fd7dc0fb8a36b660e2564703773d31acfc480c85
ChenDelong1999 <673939938@qq.com>
2024-09-21 19:42:37 +0800

    import ShareGPT4V in data __init__.py

--- data/__init__.py
+++ data/__init__.py
@@ -2,6 +2,7 @@ from .imagenet import ImageNet
 from .coco import CocoCaptionDataset
 from .image_paragraph_captioning import ImageParagraphCaptioning
 from .clevr_caption import CLEVRCaption
+from .sharegpt4v import ShareGPT4V
 
 
 def get_dataset(dataset_name, dataset_root, split='train', max_samples=None):
@@ -17,6 +18,9 @@ def get_dataset(dataset_name, dataset_root, split='train', max_samples=None):
     
     elif dataset_name == 'clevr_caption':
         dataset = CLEVRCaption(root=dataset_root, split=split, max_samples=max_samples)
+
+    elif dataset_name == 'sharegpt4v':
+        dataset = ShareGPT4V(root=dataset_root, split=split, max_samples=max_samples)
     
     else:
         raise NotImplementedError
