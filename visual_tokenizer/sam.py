c1230e73c95c9aeebbab3e591f8f82e2d1f2eb50
ChenDelong1999 <673939938@qq.com>
2024-11-01 16:08:52 +0000

    add more SAM models

--- visual_tokenizer/sam.py
+++ visual_tokenizer/sam.py
@@ -6,25 +6,34 @@ from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 
 class SAMTokenizer:
 
-    def __init__(self, image_resolution, max_tokens, device="cuda", weights_cache_dir="", **kwargs):
+    def __init__(self, checkpoint, image_resolution, max_tokens, device="cuda", weights_cache_dir="", AMG_kwargs=None, **kwargs):
         self.image_resolution = image_resolution
         self.max_tokens = max_tokens
         self.device = device
 
         # Load SAM model
-        sam_checkpoint = os.path.join(weights_cache_dir, "sam/sam_vit_h_4b8939.pth")
-        sam_model_type = "vit_h"
+        sam_checkpoint = os.path.join(weights_cache_dir, f"sam/{checkpoint}.pth")
+        if "vit_h" in checkpoint:
+            sam_model_type = "vit_h"
+        elif "vit_l" in checkpoint:
+            sam_model_type = "vit_l"
+        elif "vit_b" in checkpoint:
+            sam_model_type = "vit_b"
+        else:
+            raise ValueError("Invalid SAM model checkpoint. Checkpoint should contain 'vit_h', 'vit_l', or 'vit_b'.")
+
         self.sam = sam_model_registry[sam_model_type](checkpoint=sam_checkpoint).to(device=self.device)
         
         # Initialize mask generator with SAM model
         self.mask_generator = SamAutomaticMaskGenerator(
             model=self.sam,
-            points_per_side=32,
-            pred_iou_thresh=0.86,
-            stability_score_thresh=0.92,
-            crop_n_layers=1,
-            crop_n_points_downscale_factor=2,
-            min_mask_region_area=100,
+            **AMG_kwargs,
+            # points_per_side=32,
+            # pred_iou_thresh=0.88,
+            # stability_score_thresh=0.95,
+            # crop_n_layers=0,
+            # crop_n_points_downscale_factor=1,
+            # min_mask_region_area=0,
         )
 
     def load_images(self, images):
@@ -50,7 +59,23 @@ class SAMTokenizer:
 
         for i, img in enumerate(images):
             anns = self.mask_generator.generate(np.array(img))
-            masks = np.array([ann['segmentation'] for ann in anns]) * 1.0
+            masks = np.array([ann['segmentation'] for ann in anns])
+            masks = sam_post_processing(masks)
+
             batch_masks[i, :len(masks)] = masks[:self.max_tokens]
 
         return torch.tensor(batch_masks)
+
+
+def sam_post_processing(masks):
+
+    # find the background mask that is not covered by any instance mask
+    bg_mask = ~np.any(masks, axis=0)
+    masks = np.concatenate([masks, bg_mask[None, ...]], axis=0)
+
+    # sort masks by area
+    areas = np.sum(masks, axis=(1, 2))
+    sorted_indices = np.argsort(areas)[::-1]
+    masks = masks[sorted_indices]
+
+    return masks
